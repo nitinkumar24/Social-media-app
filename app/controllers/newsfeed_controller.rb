06a2b82a71aa0c6053812cc1b4b130bd3103d359
nitin kumar <nitin241196@gmail.com>
2017-08-29 23:28:50 +0530

    designed signin page

--- app/controllers/newsfeed_controller.rb
+++ app/controllers/newsfeed_controller.rb
@@ -1,17 +1,16 @@
-class NewsfeedController < ApplicationController
+    class NewsfeedController < ApplicationController
 
-  before_action :authenticate_user!
+      before_action :authenticate_user!
 
-  def index
-    respond_to do |format|
-      format.html{
-
+      def index
+        respond_to do |format|
+          format.html{
             @post = Post.new
             @comment=Comment.new
             @comments = Comment.all
             @feed = current_user.feed.limit(50)
-      }
-      format.js{
+          }
+          format.js{
             offset = params["offset"]
             if offset
               offset = offset.to_i
@@ -22,18 +21,17 @@ class NewsfeedController < ApplicationController
             @new_offset = offset + 10
             @show_load_more = offset < current_user.feed.count
             @feed = current_user.feed.offset(offset).limit(10)
-      }
+          }
+        end
+
+      end
+
+      def users
+        @users = User.all
+      end
+
+      def ajax
+        render :json => {text: "text"}
+      end
+
     end
-
-  end
-
-  def users
-    @users = User.all
-
-  end
-
-  def ajax
-  	render :json => {text: "text"}
-  end
-
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,29 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+				 :recoverable, :rememberable, :trackable, :validatable, :confirmable
 # validates :name, presence: true
-has_attached_file :avatar, :styles => { :medium => "1000x00>", :thumb => "40x40#" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
- 	
+	has_attached_file :avatar, :styles => { :medium => "1000x00>", :thumb => "40x40#" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 
-		def feed
-		      Post.all.order(created_at: :desc)
-		end
 
-		def generate_access_token
+
+	def feed
+		Post.all.order(created_at: :desc)
+	end
+
+	def generate_access_token
+		generated = SecureRandom.hex
+
+		until User.where(access_token: generated).first.nil?
 			generated = SecureRandom.hex
-			
-		    until User.where(access_token: generated).first.nil?
-		      generated = SecureRandom.hex
-		    end
-		    self.access_token = generated
-		    save!
 		end
+		self.access_token = generated
+		save!
+	end
 
-		
+
 
 end
