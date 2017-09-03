9c22bb9f801ddea50278ce63d471f383f6d8ecd4
nitin kumar <nitin241196@gmail.com>
2017-09-03 17:35:56 +0530

    sign in page responsive

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/sign_up
   def new
-      super
+    super
   end
 
   # POST /resource
@@ -15,25 +15,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
     collegename=params["college"]
     college=College.find_by_name(collegename)
 
-  if college.domain==domain
+    if college.domain==domain
       resource.collegename=collegename
-        if resource.save
-            if resource.active_for_authentication?
-              set_flash_message :notice, :signed_up if is_navigational_format?
-              sign_up(resource_name, resource)
-              respond_with resource, :location => after_sign_up_path_for(resource)
-            else
-              set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-              respond_with resource, :location => after_sign_up_path_for(resource)
-            end
+      if resource.save
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up if is_navigational_format?
+          sign_up(resource_name, resource)
+          respond_with resource, :location => after_sign_up_path_for(resource)
         else
-          clean_up_passwords
-          respond_with resource
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+          respond_with resource, :location => after_sign_up_path_for(resource)
         end
+      else
+        clean_up_passwords
+        respond_with resource
+      end
 
-  else
-    return redirect_to '/users/sign_up'
-  end
+    else
+      return redirect_to '/users/sign_up'
+    end
 
 
 
@@ -70,16 +70,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-            u.permit(:name,:email,:password,:password_confirmation,:collegename,:birthday,:sex,:batchof)
-            end
-
-
-  end
-  def configure_account_update_params
-          devise_parameter_sanitizer.permit(:account_update) do |u|
-            u.permit(:name, :email, :password, :current_password, :avatar)
-            end
+      u.permit(:name,:email,:password,:password_confirmation,:collegename,:birthday,:sex,:batchof)
     end
+  end
+
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password, :avatar)
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
