d816da20d811f0f521f3bd0276c4841359b08f3b
Nitin Kumar <nitin241196@gmail.com>
2017-11-11 18:26:34 +0530

    before moving to github from bitbucket

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,37 +1,36 @@
 class LikesController < ApplicationController
+    
+    before_action :authenticate_user!
+    
+    def toggle_like
+        @post = Post.find(params[:post_id])
+        like = Like.where(user: current_user, post: @post).first
+        dislike = Dislike.where(user: current_user, post: @post).first
+        notification=Notification.where(noti_type_id: @post.id).first
+        
+        if like
+            like.destroy!
+            # notification.destroy!
+            @is_liked = false
+        else
+            Like.create(user: current_user, post: @post)
+            @is_liked = true
+            
+            if @post.user_id!=current_user.id
+                Notification.create(user_id: current_user.id, recipient_id: @post.user_id, message: current_user.name + " liked your post", noti_type: "post-like", noti_type_id: @post.id)
+            end
+            
+            if dislike
+                dislike.destroy!
+                @is_disliked = false
+            end
+        
+        end
+        
+        respond_to do |format|
+            format.js { }
+        end
+    
+    end
 
-	before_action :authenticate_user!
-
-	def toggle_like
-		if params[:type]=="post"
-			@post = Post.find(params[:post_id])
-			like = Like.where(user: current_user, post: @post).first
-			dislike = Dislike.where(user: current_user, post: @post).first
-			notification=Notification.where(noti_type_id: @post.id).first
-
-			if like
-				like.destroy!
-				# notification.destroy!
-				@is_liked = false
-			else
-				Like.create(user: current_user, post: @post)
-				@is_liked = true
-
-				if @post.user_id!=current_user.id
-					Notification.create(user_id: current_user.id, recipient_id: @post.user_id, message: current_user.name + " liked your post", noti_type: "post-like", noti_type_id: @post.id)
-				end
-
-				if dislike
-					dislike.destroy!
-					@is_disliked = false
-				end
-
-			end
-
-			respond_to do |format|
-				format.js { }
-			end
-
-		end
-	end
 end
--- app/controllers/newsfeed_controller.rb
+++ app/controllers/newsfeed_controller.rb
@@ -1,94 +1,103 @@
 class NewsfeedController < ApplicationController
-
-  before_action :authenticate_user!
-
-  def index
-    respond_to do |format|
-      format.html{
-        @post = Post.new
+    
+    before_action :authenticate_user!
+    
+    def index
+        respond_to do |format|
+            format.html{
+                @post = Post.new
+                @comment=Comment.new
+                @comments = Comment.all
+                @feed = current_user.feed.limit(50)
+                @users=User.first(5)
+            }
+            format.js{
+                offset = params["offset"]
+                if offset
+                    offset = offset.to_i
+                else
+                    offset = 0
+                end
+                
+                @new_offset = offset + 10
+                @show_load_more = offset < current_user.feed.count
+                @feed = current_user.feed.offset(offset).limit(10)
+            }
+        end
+    
+    end
+    
+    def users
+        domain=current_user.email.split('@').last
+        puts domain
+        @users = User.all.order(name: :desc)
+    end
+    
+    def profile
+        @posts=Post.where(user_id:params[:user_id]).order(created_at: :DESC)
         @comment=Comment.new
         @comments = Comment.all
-        @feed = current_user.feed.limit(50)
-      }
-      format.js{
-        offset = params["offset"]
-        if offset
-          offset = offset.to_i
-        else
-          offset = 0
+        user_id=params[:user_id]
+        @user=User.find_by_id(user_id)
+    end
+    
+    
+    def friendrequests
+        @friendrequests=Friendrequest.where(receiver_id: current_user.id)
+    end
+    
+    
+    def follow
+        followee_id = params[:followee_id]
+        @user = User.find(followee_id)
+        puts @user
+        if current_user.can_follow followee_id
+            puts "in follow"
+            Friendrequest.create(:receiver_id => followee_id,:sender_id => current_user.id)
+            # FollowMapping.create(:followee_id => followee_id, :follower_id => current_user.id)
         end
-
-        @new_offset = offset + 10
-        @show_load_more = offset < current_user.feed.count
-        @feed = current_user.feed.offset(offset).limit(10)
-      }
+        # respond_to do |format|
+        #     format.js { }
+        # end
+        return redirect_to '/users'
     end
-
-  end
-
-  def users
-    @users = User.all.order(name: :desc)
-  end
-
-  def profile
-    @posts=Post.where(user_id:params[:user_id]).order(created_at: :DESC)
-    @comment=Comment.new
-    @comments = Comment.all
-    user_id=params[:user_id]
-    @user=User.find_by_id(user_id)
-  end
-
-
-  def friendrequests
-    @friendrequests=Friendrequest.where(receiver_id: current_user.id)
-  end
-
-
-  def follow
-    followee_id = params[:followee_id]
-    if current_user.can_follow followee_id
-      puts "in follow"
-      Friendrequest.create(:receiver_id => followee_id,:sender_id => current_user.id)
-      # FollowMapping.create(:followee_id => followee_id, :follower_id => current_user.id)
-    else
+    
+    def delete_request
+        followee_id = params[:followee_id]
+        if current_user.can_delete_request followee_id
+            Friendrequest.where(:receiver_id => followee_id, :sender_id => current_user.id).first.destroy
+        else
+        end
+        return redirect_to '/users'
     end
-    return redirect_to '/users'
-  end
-
-  def un_follow
-    followee_id = params[:followee_id]
-    if current_user.can_un_follow followee_id
-      FollowMapping.where(:followee_id => followee_id, :follower_id => current_user.id).first.destroy
-    else
+    
+    def un_follow
+        followee_id = params[:followee_id]
+        if current_user.can_un_follow followee_id
+            FollowMapping.where(:followee_id => followee_id, :follower_id => current_user.id).first.destroy
+        else
+        end
+        return redirect_to '/users'
     end
-    return redirect_to '/users'
-  end
-
-  def delete_request
-    followee_id = params[:followee_id]
-    if current_user.can_delete_request followee_id
-      Friendrequest.where(:receiver_id => followee_id, :sender_id => current_user.id).first.destroy
-    else
+    
+    
+    
+    def accept_request
+        follower_id = params[:follower_id]
+        FollowMapping.create(:followee_id => current_user.id, :follower_id => follower_id)
+        Friendrequest.where(:receiver_id => current_user.id, :sender_id => follower_id).first.destroy
+        return redirect_to '/users'
     end
-    return redirect_to '/users'
-  end
-
-  def accept_request
-    follower_id = params[:follower_id]
-    FollowMapping.create(:followee_id => current_user.id, :follower_id => follower_id)
-    Friendrequest.where(:receiver_id => current_user.id, :sender_id => follower_id).first.destroy
-    return redirect_to '/users'
-  end
-
-  def reject_request
-    follower_id = params[:follower_id]
-    Friendrequest.where(:receiver_id => current_user.id, :sender_id => follower_id).first.destroy
-    return redirect_to '/users'
-  end
-
-
-  def ajax
-    render :json => {text: "text"}
-  end
+    
+    def reject_request
+        follower_id = params[:follower_id]
+        Friendrequest.where(:receiver_id => current_user.id, :sender_id => follower_id).first.destroy
+        return redirect_to '/users'
+    end
+    
+    
+    def ajax
+        render :json => {text: "text"}
+    end
 
 end
