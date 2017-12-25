9742b17ef912a58ba4e7c0c60c1502c9c73a0d8b
Nitin Kumar <nitin241196@gmail.com>
2017-12-26 01:18:05 +0530

    chaged in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+          address:              'smtp.gmail.com',
+          port:                 587,
+          domain:               'gmail.com',
+          user_name:            'nitin241196@gmail.com',
+          password:             'NitZ.24@GmaiL',
+          authentication:       :plain,
+          enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: '18.216.7.6', port: 80 }
+
 end
