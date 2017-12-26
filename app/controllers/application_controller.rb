37bc43614f301299efb49249e2681d89ba1cdd75
Nitin Kumar <nitin241196@gmail.com>
2017-12-26 18:14:14 +0530

    minr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_raven_context
+
 
 
   def configure_permitted_parameters
@@ -12,5 +14,10 @@ class ApplicationController < ActionController::Base
     end
 
   end
+  private
 
+  def set_raven_context
+      Raven.user_context(id: session[:current_user_id]) # or anything else in session
+      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end
