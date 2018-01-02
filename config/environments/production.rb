6c996a2b3504fbe5be3cf3bf7e34adc9d6a4381b
Nitin Kumar <nitin241196@gmail.com>
2018-01-03 01:40:39 +0530

    domain for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: '18.216.7.6', port: 80 }
+  config.action_mailer.default_url_options = { host: 'sociograms.in', port: 80 }
 
   config.action_mailer.smtp_settings = {
           address:              'smtp.gmail.com',
