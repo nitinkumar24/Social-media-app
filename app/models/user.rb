2979fa0489ff1c3c5ad45704a1d64df5a5fa835f
Nitin Kumar <nitin241196@gmail.com>
2018-01-04 02:06:32 +0530

    edit profile without current password

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,27 @@ class User < ApplicationRecord
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
     has_many :posts, dependent: :destroy
 
+    def update_with_password(params, *options)
+        current_password = params.delete(:current_password)
+
+        if params[:password].blank?
+            params.delete(:password)
+            params.delete(:password_confirmation) if params[:password_confirmation].blank?
+        end
+
+        result = if params[:password].blank? || valid_password?(current_password)
+                     update_attributes(params, *options)
+                 else
+                     self.assign_attributes(params, *options)
+                     self.valid?
+                     self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+                     false
+                 end
+
+        clean_up_passwords
+        result
+    end
+
     def to_param
         [id, name.parameterize].join("-")
     end
@@ -91,7 +112,5 @@ class User < ApplicationRecord
         self.access_token = generated
         save!
     end
-
-
 
 end
