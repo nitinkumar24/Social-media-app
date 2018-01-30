6d784bcde1d80ff166a1ce5061a4c6f9e4af7cf0
Nitin Kumar <nitin241196@gmail.com>
2018-01-30 16:14:27 +0530

    added open_emails

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
-    protected
+
+    def create
+        super
+        open_emails = ["gmail.com","outlook.com","yahoo.com","hotmail.com"]
+        user_mail_domain = resource.email.split("@").last
+        if open_emails.include? user_mail_domain
+            puts "open"
+        else
+            puts "close"
+        end
+
+    end
+
 end
