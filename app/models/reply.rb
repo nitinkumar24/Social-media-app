1f6c8ea7721c86b37bce10a227a475c8b3b3771b
Nitin Kumar <nitin241196@gmail.com>
2018-02-20 20:23:13 +0530

    added authorization for reply edit and delete

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,12 @@
 class Reply < ApplicationRecord
   belongs_to :user
   belongs_to :comment
+
+  def can_delete user
+      self.user_id == user.id or self.comment.user_id == user.id or self.comment.post.user_id == user.id
+  end
+
+  def can_update user
+      self.user_id == user.id
+  end
 end
