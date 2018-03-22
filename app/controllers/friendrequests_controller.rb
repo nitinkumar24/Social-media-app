ab15f87d59a37e58057558f86c72bc4aca0eced4
Nitin Kumar <nitin241196@gmail.com>
2018-03-22 23:37:04 +0530

    notification page css improved

--- app/controllers/friendrequests_controller.rb
+++ app/controllers/friendrequests_controller.rb
@@ -36,7 +36,7 @@ class FriendrequestsController < ApplicationController
         link_to_actor_profile = current_user.profile_link current_user                     #actor is current_user
         Notification.create(user_id: current_user.id, recipient_id: follower_id,
                             message: link_to_actor_profile + " Accepted your follow request",
-                            noti_type: "request accept",
+                            noti_type: 'request accept',
                             mode:current_user.current_mode)
         u = User.find_by_id(follower_id)
         u.new_notifications += 1
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -14,26 +14,26 @@ class NotificationsController < ApplicationController
             @comment_to_show = []
             @comment_to_show << Comment.where(id: noti_type_id).first       #because @comment k lie edit wala form aa ja rha h, islie @comments
             @post_to_show = @comment_to_show[0].post
-        end
-        if noti_type == "comment-reply" or noti_type == "reply-mention"
+
+        elsif noti_type == "comment-reply" or noti_type == "reply-mention"
             @comment_to_show = []
             @reply = Reply.where(id: noti_type_id).first
             @comment_to_show << @reply.comment
             @post_to_show = @comment_to_show[0].post
-        end
 
-        if noti_type == "post-like"
+        elsif noti_type == "post-like"
             @comment_to_show = []
             @like = Like.where(id: noti_type_id).first
             @post_to_show = @like.post
             @comment_to_show << @post_to_show.comments
 
-        end
-
-        if noti_type ==  "post-mention"
+        elsif noti_type ==  "post-mention"
             @comment_to_show = []
             @post_to_show = Post.where(id: noti_type_id).first
             @comment_to_show << @post_to_show.comments
+
+        else
+            redirect_to '/' , notice: 'something went wrong'
         end
 
         @comment = Comment.new
