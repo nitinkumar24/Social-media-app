2dd552547f09a772828fff04eb25eade11364c00
Nitin Kumar <nitin241196@gmail.com>
2018-04-01 23:43:33 +0530

    modified mention css and only showing name in suggestion

--- app/assets/javascripts/mention.js
+++ app/assets/javascripts/mention.js
@@ -3,7 +3,7 @@ this.Post = class Post {
         return $('#post_content').atwho({
             at: '@',
             searchKey: 'real_name',
-            displayTpl: "<li class='mention-item' </li>" + "<img class=\"h-pro-image\" alt=\"\" src=${image}>" + "<span  data-value='(${name},${real_name},)'>${name}${real_name}" + "<br>",
+            displayTpl: "<li class='mention-item' </li>" + "<img class=\"h-pro-image\" alt=\"\" src=${image}>" + " "+ "<span  data-value='(${real_name},)'>${real_name}" + "<br>",
             callbacks: {
                 remoteFilter: function(query, callback) {
                     if (query.length < 1) {
