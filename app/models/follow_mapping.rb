fa5e570bebb8cea46ad7149daa29e97a536a50aa
Nitin Kumar <nitin241196@gmail.com>
2018-04-25 02:50:45 +0530

    removed uniqueness from followmapping and friendreuest

--- app/models/follow_mapping.rb
+++ app/models/follow_mapping.rb
@@ -1,4 +1,2 @@
 class FollowMapping < ApplicationRecord
-    validates_uniqueness_of :followee_id, scope: :follower_id
-
 end
--- app/models/friendrequest.rb
+++ app/models/friendrequest.rb
@@ -1,5 +1,2 @@
 class Friendrequest < ApplicationRecord
-
-    validates_uniqueness_of :receiver_id, scope: :sender_id
-
 end
