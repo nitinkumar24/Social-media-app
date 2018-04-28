0a1c7d681f9d714cedfd6c057fe1bec3b6eba428
Nitin Kumar <nitin241196@gmail.com>
2018-04-28 17:19:40 +0530

    minor css changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
 
 
 
-    has_attached_file :avatar, :styles => { :thumb => '160x160', :original => '960x960>'}, :default_url => "https://png.icons8.com/dusk/100/000000/gender-neutral-user.png"
+    has_attached_file :avatar, :styles => { :thumb => '200x200>', :original => '700x700>'}, :default_url => "https://png.icons8.com/dusk/100/000000/gender-neutral-user.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
     crop_attached_file :avatar
     devise :omniauthable, :omniauth_providers => [:google_oauth2]
